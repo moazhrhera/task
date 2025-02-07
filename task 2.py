b58d1940f5890248a2c89f66d7b8b70d253f9bf5
Eng-Moaz01 <mmezohrhera@gmail.com>
2025-02-07 21:27:57 +0200

    While

--- task 2.py
+++ task 2.py
@@ -1,20 +1,28 @@
-A = float(input("Enter the first number ->"))
-B = float(input("Enter the first number ->"))
-operation = input("Enter the operation (*,/,-,+)")
+program = True
+while program:
+    A = str(input("Enter the first number  or To close Enter L ->"))
+    if A == "L":
+        print("program closed")
+        program= False
+        break
+    
+    B = float(input("Enter the second number ->"))
+    operation = input("Enter the operation (*,/,-,+)")
 
 
-if operation == "/":
-    if B == 0:
-        print("Erorr:Division by zero is not allowed!")
-    else:
-        result = A / B
+    if operation == "/":
+        if B == 0:
+            print("Erorr:Division by zero is not allowed!")
+        else:
+            result = A / B
+            print("result =", result)
+    elif operation == "*":
+        result = A * B 
         print("result =", result)
-elif operation == "*":
-    result = A * B 
-    print("result =", result)
-elif operation == "-":
-    result = A-B
-    print("result =", result)
-elif operation == "+":
-    result = A+B
-    print("result =", result)
+    elif operation == "-":
+        result = A-B
+        print("result =", result)
+    elif operation == "+":
+        result = A+B
+        print("result =", result)
+    
